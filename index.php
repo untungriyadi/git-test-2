74c2c57984d2946c8a5c36cf9435e4c3aee5ef5f
Untung Riyadi <untngrydh@gmail.com>
2021-10-29 16:50:28 +0700

    add list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 </head>
 <body>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam repudiandae ea aliquam iure voluptate ducimus officia suscipit sed, saepe possimus reiciendis officiis voluptatem dolorem explicabo natus! Voluptatem laudantium corrupti nulla nemo. At rerum laboriosam amet, corrupti, assumenda, iure voluptatem quos voluptatum provident adipisci cupiditate accusamus iusto inventore rem delectus unde tempore. Autem inventore, cum sit debitis fugiat, tempora veritatis nam fuga pariatur distinctio ipsum ipsa vero. Consectetur, delectus quam. Recusandae magni ullam animi mollitia maxime blanditiis maiores minus doloremque incidunt dolorum quam voluptatibus dolores facilis esse laboriosam illo quidem, tenetur at quas? Ipsa libero delectus nisi odit, debitis vel accusantium!</p>
+    <ul>
+        <li>Cobaaaa </li>
+    </ul>
 </body>
 </html>
