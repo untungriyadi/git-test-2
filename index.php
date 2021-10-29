f4cff0b1f42fcc1be524ffad14f8ed3c4ab24aa4
Untung Riyadi <untngrydh@gmail.com>
2021-10-29 16:46:31 +0700

    add p

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
     <title>Coba GIT Remote</title>
 </head>
 <body>
-    
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam repudiandae ea aliquam iure voluptate ducimus officia suscipit sed, saepe possimus reiciendis officiis voluptatem dolorem explicabo natus! Voluptatem laudantium corrupti nulla nemo. At rerum laboriosam amet, corrupti, assumenda, iure voluptatem quos voluptatum provident adipisci cupiditate accusamus iusto inventore rem delectus unde tempore. Autem inventore, cum sit debitis fugiat, tempora veritatis nam fuga pariatur distinctio ipsum ipsa vero. Consectetur, delectus quam. Recusandae magni ullam animi mollitia maxime blanditiis maiores minus doloremque incidunt dolorum quam voluptatibus dolores facilis esse laboriosam illo quidem, tenetur at quas? Ipsa libero delectus nisi odit, debitis vel accusantium!</p>
 </body>
 </html>
